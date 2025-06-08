d35680639fe16f26f3a83782111bcb7ad77a90d0
arun <arunkumar413@gmail.com>
2025-06-08 15:13:54 +0530

    addding variables, maps and structs

---
+++ variables/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// signed integers
+	var a int = 32
+	var b int8 = 16
+	var c int16 = 554
+	var d int32 = 3535345
+	var e int64 = 54353534534
+
+	// unsigned integers
+	var f uint = 888
+	var g uint16 = 45454
+	var h uint32 = 454549534
+	var i uint64 = 455353453435
+	var j uint8 = 45
+
+	fmt.Println(a, b, c, d, e, f, g, h, i, j)
+}
